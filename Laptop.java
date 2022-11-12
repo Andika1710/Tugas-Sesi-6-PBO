39bb125d39f8eee8a0a61aa8895678f81e82c38a
Andhika Oktasandira <114379148+Andika1710@users.noreply.github.com>
2022-11-12 23:18:54 +0700

    Add files via upload

---
+++ Laptop.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laptop;
+
+/**
+ *
+ * @author Axioo
+ */
+public class Laptop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    static void add(laptop laptop) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
